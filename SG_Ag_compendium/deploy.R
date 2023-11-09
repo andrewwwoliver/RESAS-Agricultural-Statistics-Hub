8f11bacc21cf2bf520d5b67f076b7bc234f70d7c
Jackie Massaya <113352950+jckmssy@users.noreply.github.com>
2023-11-09 14:31:49 +0000

    Delete SG_Ag_compendium/deploy.R

--- SG_Ag_compendium/deploy.R
+++
@@ -1,7 +0,0 @@
-rsconnect::setAccountInfo(name='colinstevensonsg',
-                          token='2F36EE0EB310F91D01F1D91B8C5DB011',
-                          secret='IxxQ3NdFNprwMpelGWXE6dk9jlBMBRs2Ejxz8Q20')
-
-
-library(rsconnect)
-rsconnect::deployApp("C:/Users/u456287/OneDrive - SCOTS Connect/Compendium Project/Shiny Apps/agriculture_compendium_dashboard_main/agriculture_compendium_dashboard_main/deploy")
